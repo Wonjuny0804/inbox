e60d133aef703bf9183d258a03ebb3b657e42459
Wonjun Jang <wonwonjun@gmail.com>
2022-02-02 22:06:14 +0900

    First smart contract and write compile

--- compile.js
+++ compile.js
@@ -1,1 +1,9 @@
 // compile code will go here
+const path = require("path");
+const fs = require("fs");
+const solc = require("solc");
+
+const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
+const source = fs.readFileSync(inboxPath, { encoding: "utf-8" });
+
+console.log(solc.compile(source, 1));
